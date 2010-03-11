9303e29bbe19d3a681c6537003dc6286483a4d57
James Sanders <james.sanders@thisismedium.com>
2010-03-10 20:10:13 -0500

    Added group matching feature to rename.

--- src/rename.hs
+++ src/rename.hs
@@ -1,35 +1,45 @@
+import Control.Exception (bracket)
+import Control.Monad
 import Data.String.Utils
 import System.Directory
 import System.Environment
 import System.FilePath
 import System.Find
 import System.Process
+import System.Exit
+import System.IO
 import Text.Printf
-import Text.Regex.Posix
 import Text.Regex.Posix.Wrap
 
+main :: IO ()
 main = do
   args <- getArgs
+  when (length args < 3) (usage >> exitFailure)
   let ra = reverse . take 3 . reverse $ args
-  let rx = ra !! 0
+  let rx = head ra
   let rt = ra !! 1
   let path = ra !! 2
-  putStrLn $ printf " + Replace '%s' with '%s' in '%s'" rx rt path
   ------------------------------------------------------------------------
   files <- find path doesFileExist
   dirs  <- find path doesDirectoryExist
-  mapM (doReplace rx rt) files
-  mapM (doReplace rx rt) dirs
+  mapM_ (doReplace rx rt) files
+  mapM_ (doReplace rx rt) dirs
+
+numReplace :: [[Char]] -> [Char] -> [Char]
+numReplace ls str = replaceNumbered ls str 1
+                    where
+                      replaceNumbered [] str' _ = str'
+                      replaceNumbered (x:xs) str' n = replace ("%" ++ show n) x (replaceNumbered xs str' (n+1))
 
 doReplace regex replacement fpath = do
   let fname = takeFileName  fpath
   let dname = takeDirectory fpath
   let checkName = fname =~ regex
-  if checkName 
-     then do
-       let found  = fname =~ regex
-       let rnamed = dname </> replace found replacement fname
-       askPermission 
+  when checkName $
+     do let found   = fname =~ regex :: [[String]]
+        let rep     = numReplace (tail $ head found) replacement
+        let rnamed  = dname </> replace (head $ head found) rep fname
+        askPermission 
           (printf "Move '%s' to '%s'?" fpath rnamed) 
           (do fe <- doesFileExist rnamed
               de <- doesDirectoryExist rnamed
@@ -41,21 +51,23 @@ doReplace regex replacement fpath = do
                             then renameDirectory fpath rnamed
                             else renameFile fpath rnamed) 
           (putStrLn " - Skipping")
-     else return ()
+     
+getChar' :: IO Char
+getChar' = bracket 
+              (system "stty raw"  >> system "stty -echo")
+              (\_-> system "stty sane" >> system "stty echo")
+              (\_-> getChar)
 
 
+askPermission :: String -> IO () -> IO () -> IO ()
 askPermission str ifyes ifno = do
   putStrLn (str ++ " (y/n)")
-  system "stty raw"
-  system "stty -echo"
-  answer <- getChar
-  system "stty sane"
-  system "stty echo"
-  if (not $ answer `elem` "yn") 
-    then do putStrLn "Answer must be 'y' or 'n'." 
-            askPermission str ifyes ifno
-    else if answer == 'y'
-           then ifyes
-           else ifno
-  
-  
+  answer <- getChar'
+  if (answer `notElem` "yn")
+     then do putStrLn "Answer must be 'y' or 'n'." 
+             askPermission str ifyes ifno
+     else if (answer == 'y') then ifyes else ifno 
+
+
+usage = do progname <- getProgName
+           hPutStrLn stderr $ printf "Usage: %s <find_regex> <replacement> <filepath>" progname
